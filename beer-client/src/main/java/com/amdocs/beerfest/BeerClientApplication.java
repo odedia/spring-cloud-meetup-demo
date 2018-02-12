764f3b78e14c6e91195de49561f450dd167236aa
Oded Shopen <so523k@us.att.com>
2018-02-12 15:57:10 +0200

    with zipkin and hystrix

--- beer-client/src/main/java/com/amdocs/beerfest/BeerClientApplication.java
+++ beer-client/src/main/java/com/amdocs/beerfest/BeerClientApplication.java
@@ -1,31 +1,46 @@
 package com.amdocs.beerfest;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableBinding(Source.class)
+@EnableCircuitBreaker
+@Slf4j
 public class BeerClientApplication {
 
 	public static void main(String[] args) {
@@ -34,7 +49,8 @@ public class BeerClientApplication {
 
 	@GetMapping("/beer/{name}")
 	public String getBeer(@PathVariable String name){
-		return "Hi " + name + "! Have a Heiniken.";
+		log.info("Entered greeting API");
+		return "Hi! Have a " + name + "!";
 	}
 }
 
@@ -49,11 +65,29 @@ class Config{
 }
 
 @RestController
+@Slf4j
 class BeerNamesController {
+	@Output(Source.OUTPUT)
+	@Autowired
+	private MessageChannel messageChannel;
+
+	
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@PostMapping("/beer-me-up")
+	public void write(@RequestBody BeerDTO beer){
+		log.info("Beer is {}", beer);
+		this.messageChannel.send(MessageBuilder.withPayload(beer.getName()).build());
+	}
+	
+	
+	public List<String> getBeerNamesFallback(){
+		return Arrays.asList("Corrona");
+	}
+	
 	@GetMapping("/beer-names")
+	@HystrixCommand(fallbackMethod="getBeerNamesFallback")
 	public List<String> getBeerNames(){
 		return restTemplate
 									.exchange("http://beer-service/beers", 
--- beer-service/src/main/java/com/amdocs/beermanager/BeermanagerApplication.java
+++ beer-service/src/main/java/com/amdocs/beermanager/BeermanagerApplication.java
@@ -6,25 +6,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(Sink.class)
 public class BeermanagerApplication {
 
 	@Bean
@@ -44,15 +52,30 @@ public class BeermanagerApplication {
 	}
 }
 
+@Component
+@Slf4j
+class BeerHandler {
+	
+	@Autowired 
+	BeerRepository repo;
+	
+	@StreamListener(target=Sink.INPUT)
+	public void handleBeerMessage (String message) {
+		log.info("Received beer: {}", message);
+		this.repo.save(new Beer(null, message, 10.0));
+	}
+}
 
 @RestController
 @RefreshScope
+@Slf4j
 class BeerGreeting {
 	@Value("${greeting}")
 	private String greeting;
 	
 	@GetMapping("/greeting")
 	String greeting(){
+		log.info("Returning greeting from service...");
 		return greeting;
 	}
 }

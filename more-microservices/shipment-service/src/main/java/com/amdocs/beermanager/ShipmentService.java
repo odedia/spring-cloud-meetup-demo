fbf215e7827d59dbc1d39682d15c65a0f51247a3
Oded Shopen <so523k@us.att.com>
2018-03-19 14:51:26 +0200

    changes to shipment service

--- more-microservices/shipment-service/src/main/java/com/amdocs/beermanager/ShipmentService.java
+++ more-microservices/shipment-service/src/main/java/com/amdocs/beermanager/ShipmentService.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,9 +16,9 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@SpringBootApplication
 public class ShipmentService {
 
 	public static void main(String[] args) {
